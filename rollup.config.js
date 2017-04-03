e31829f65840687eb4076557665475a598c42e31
Kyle Florence <kyle.florence@gmail.com>
2017-04-02 17:49:21 -0700

    configure travis CI

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,14 @@
 import babel from "rollup-plugin-babel"
 
-// Rollup configuration
+// https://rollupjs.org/
 export default {
   entry: "src/library.js",
-  format: "umd",
-  moduleName: "library",
   plugins: [
     babel({ exclude: "node_modules/**" })
   ],
   sourceMap: true,
   targets: [
-    { dest: "dist/library.js", format: "umd" },
+    { dest: "dist/library.js", format: "umd", moduleName: "library" },
     { dest: "dist/library.es.js", format: "es" }
   ]
 }
